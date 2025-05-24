cb53ff4c6adae1407f7462555453f03742d2e07b
Amdubaloch <amdubaloch2005@gmail.com>
2025-05-24 23:56:12 +0500

    chatbot

---
+++ hello.py
@@ -0,0 +1,46 @@
+import os
+import chainlit as cl
+from dotenv import find_dotenv, load_dotenv
+from agents import Agent, RunConfig, AsyncOpenAI, OpenAIChatCompletionsModel, Runner
+
+load_dotenv(find_dotenv())
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=provider,
+)
+
+run_config = RunConfig(
+    model=model,
+    model_provider=provider,
+    tracing_disabled=True,
+)
+
+agent1 = Agent(
+    instructions="You are a helpful assistant that can answer questions and help with tasks.",
+    name="Assistant",
+)
+
+@cl.on_chat_start
+async def hendle_chat_start():
+    cl.user_session.set("history", [])
+    await cl.Message(content="Hello! How can I assist you today?").send()
+
+
+@cl.on_message
+async def handle_message(message: cl.Message):
+    history = cl.user_session.get("history")
+    history.append({"role": "user", "content": message.content})
+    result = await Runner.run(
+    agent1,
+    input=message.content,
+    run_config=run_config
+    )
+    await cl.Message(content=result.final_output).send()
