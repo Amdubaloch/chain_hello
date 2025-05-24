dde9c1c39eaeada55016bbf986ed641a05bf4856
Amdubaloch <amdubaloch2005@gmail.com>
2025-05-25 00:12:03 +0500

    update

--- hello.py
+++ hello.py
@@ -37,10 +37,14 @@ async def hendle_chat_start():
 @cl.on_message
 async def handle_message(message: cl.Message):
     history = cl.user_session.get("history")
+
+
     history.append({"role": "user", "content": message.content})
     result = await Runner.run(
     agent1,
-    input=message.content,
-    run_config=run_config
+    input=history,
+    run_config=run_config,
     )
+    history.append({"role": "assistant", "content": result.final_output})
+    cl.user_session.set("history", history)
     await cl.Message(content=result.final_output).send()
